26402c518cf85915236b8613fcb9787af19982b9
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-23 13:20:52 -0300

    refactor: make select component dynamic

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,26 +1,27 @@
-import React, { ChangeEvent } from 'react';
-import { useBreed } from '../../hooks/useBreed';
+import React, { ChangeEvent, Dispatch, SetStateAction, SelectHTMLAttributes } from 'react';
 
-interface IProps {
+interface IProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: string[];
+  setState: Dispatch<SetStateAction<any>>;
 }
 
-const Select: React.FC<IProps> = ({ options, ...rest }) => {
-  const { setSelectedBreed, selectedBreed } = useBreed();
-
+const Select: React.FC<IProps> = ({ setState, options, children, ...rest }) => {
   const handleSelect = (event: ChangeEvent<HTMLSelectElement>) => {
     const { value } = event.target;
-    setSelectedBreed(value);
+    setState(value);
   };
 
   return (
-    <select {...rest} value={selectedBreed} onChange={handleSelect}>
-      {options.map(option => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <>
+      <label>{children}</label>
+      <select {...rest} onChange={handleSelect}>
+        {options.map(option => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 

b4024ba06ba04cbc85d82796fbb7fb2e86204746
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-23 13:18:38 -0300

    feat: create route to edit dogs info

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MainPage from '../pages/MainPage';
 import About from '../pages/About';
+import DogDetails from '../pages/DogDetails';
 
 const Routes: React.FC = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={MainPage} />
+      <Route exact path="/edit/:id" component={DogDetails} />
       <Route exact path="/about" component={About} />
     </Switch>
   </Router>
